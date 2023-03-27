60a2fbcf43adbb749f76e1a8fd4492880e4fbd9e
Yamkovoi Danil <danil1998.01.08@outlook.com>
2023-03-27 08:59:10 +0500

    Implement EnumShower

---
+++ Editor/CustomAttributeDrawer/ConditionalAttributeDrawer/ConditionalEnumDrawer.cs
@@ -0,0 +1,45 @@
+﻿using HelpTool.RunTime.CustomAttribute.Conditional;
+using UnityEditor;
+using UnityEngine;
+
+namespace HelpTool.Editor.CustomAttributeDrawer.ConditionalAttributeDrawer
+{
+    [CustomPropertyDrawer(typeof(ShowEnumAttribute))]
+    public class ConditionalEnumDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            SerializedProperty boolProperty = FindProperty(property);
+            if (boolProperty == null)
+            {
+                Debug.LogError("Bool property not found!");
+                return;
+            }
+
+            bool isHidden = CanHidden(boolProperty);
+            if (!isHidden)
+                EditorGUI.PropertyField(position, property, label, true);
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            SerializedProperty boolProperty = FindProperty(property);
+            if (boolProperty == null || CanHidden(boolProperty) == false)
+            {
+                return EditorGUI.GetPropertyHeight(property, label);
+            }
+
+            return 0f;
+        }
+
+        private SerializedProperty FindProperty(SerializedProperty property)
+        {
+            return property.serializedObject.FindProperty(((ShowEnumAttribute) attribute).EnumName);
+        }
+
+        private bool CanHidden(SerializedProperty boolProperty)
+        {
+            return boolProperty.enumValueIndex != ((ShowEnumAttribute) attribute).EnumValue;
+        }
+    }
+}
