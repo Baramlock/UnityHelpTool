0479116f45afaae3dec1e1290a38d4a76b9799f4
Yamkovoi Danil <danil1998.01.08@outlook.com>
2023-03-27 13:57:48 +0500

    Added Sub UnityExtension

---
+++ RunTime/Extension/UnityExtension.cs
@@ -0,0 +1,45 @@
+﻿namespace HelpTool.RunTime.Extension
+{
+    public static class UnityExtension
+    {
+        /// <summary>
+        /// Return true if object is not null
+        /// </summary>
+        /// <param name="self"></param>
+        /// <returns></returns>
+        public static bool Exist(this object self)
+        {
+            return self != null;
+        }
+
+        /// <summary>
+        /// Return true if object is not null
+        /// </summary>
+        /// <param name="self"></param>
+        /// <returns></returns>
+        public static bool Exist(this UnityEngine.Object self)
+        {
+            return self;
+        }
+
+        /// <summary>
+        /// Return true if object is null
+        /// </summary>
+        /// <param name="self"></param>
+        /// <returns></returns>
+        public static bool NotExist(this object self)
+        {
+            return self == null;
+        }
+
+        /// <summary>
+        /// Return true if object is null
+        /// </summary>
+        /// <param name="self"></param>
+        /// <returns></returns>
+        public static bool NotExist(this UnityEngine.Object self)
+        {
+            return !self;
+        }
+    }
+}
